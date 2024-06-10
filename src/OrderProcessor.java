9b0520268dfa532e647f18d3400ffdaa865af709
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2024-06-10 10:51:42 +0300

    -

--- src/OrderProcessor.java
+++ src/OrderProcessor.java
@@ -1,10 +1,12 @@
+import java.util.List;
 public class OrderProcessor {
     private PaymentService paySrv;
     private InventoryService invSrv;
+//    private Item Item;
     public OrderProcessor(PaymentService paySrv, InventoryService invSrv) {
         this.paySrv = paySrv;
         this.invSrv = invSrv;
-        boolean processOrder(paySrv PaymentDetails payDtls, List<Item> items){
+        boolean processOrder(PaymentDetails payDtls, List<Item> items){
             // Code to process the order
         }
     }
