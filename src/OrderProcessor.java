d01a6241b02c2e38c22c2f1f12b34961280e0c62
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2024-06-10 12:09:20 +0300

    order conditions added

--- src/OrderProcessor.java
+++ src/OrderProcessor.java
@@ -2,18 +2,19 @@ import java.util.List;
 public class OrderProcessor {
     private PaymentService paySrv;
     private InventoryService invSrv;
-    private Item Item;
-    public OrderProcessor(PaymentService paySrv, InventoryService invSrv) {
+    private Item item;
+    public OrderProcessor(PaymentService paySrv, InventoryService invSrv, Item item) {
         this.paySrv = paySrv;
         this.invSrv = invSrv;
+        this.item = item;
     }
     boolean processOrder(PaymentDetails payDtls, List<Item> items){
         // Code to process the order
         boolean paymentSuccess = paySrv.processPayment(payDtls);
-        boolean itemListOK = items.contains(Item.getProductId());
+        boolean itemListOK = items.contains(item.getProductId());
         if(paymentSuccess){
             invSrv.updateInventory(items);
-//            return true;
+            return true;
         }
         else if(itemListOK){
             return true;
@@ -25,6 +26,7 @@ public class OrderProcessor {
         else if(!paymentSuccess){
             return false;
         }
+
         return true;
 //        if(paySrv.processPayment(payDtls) && !items.isEmpty()){
 //            invSrv.updateInventory(items);
@@ -33,5 +35,7 @@ public class OrderProcessor {
 //        else{
 //            return false;
 //        }
+    }
+public static void main(String[] args) {
     }
 }
